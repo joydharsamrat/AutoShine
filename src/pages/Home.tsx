2398400ddecc87d517d24baf7ee09d85b16376b0
joydharsamrat <joydharsamrat@gmail.com>
2024-10-26 20:08:45 +0600

    bug fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import FeaturedServices from "../components/Home/FeaturedServices";
 import HeroSection from "../components/Home/Hero";
-import ReviewSection from "../components/Home/Review";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <FeaturedServices />
-      <ReviewSection />
     </div>
   );
 };
